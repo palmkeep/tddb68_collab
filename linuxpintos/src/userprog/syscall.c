770f2b9f8c2fd749f7c2ba6c7949b72a7f502dff
andpa149 <andpa149@student.liu.se>
2018-01-19 20:24:50 +0100

    Sys call numbers are

--- linuxpintos/src/userprog/syscall.c
+++ linuxpintos/src/userprog/syscall.c
@@ -3,7 +3,10 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+
+//Our imports
 #include "threads/init.h"
+#include "filesys/filesys.h"
 #include "lib/kernel/stdio.h"
 
 static void syscall_handler (struct intr_frame *);
@@ -17,20 +20,33 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  if (f->esp == SYS_HALT)
+  int sys_call_nr = *(int*)(f->esp);
+
+  if ( sys_call_nr == SYS_HALT )
   {
     power_off();
   }
-  else if (f->esp == SYS_CREATE)
+  else if ( sys_call_nr == SYS_CREATE )
+  {
+    if ( filesys_create(f->esp+4,f->esp+8) )
+    {
+      f->eax = 1;
+    }
+    else
+    {
+      f->eax = 0;
+    }
+  }
+  else if ( sys_call_nr == SYS_WRITE )
   {
 
-
-    // If created set f->eax = 1
-    // If not created set f->eax = 0
   }
   else
   {
-    printf ("YOU SHIT IMPLEMENT THIS SYSCALL!\n");
+    printf ("IMPLEMENT THIS SYSCALL!\n");
+    printf ("SYS_CALL_NR: %d\n", sys_call_nr);
+    printf ("SYS_CALL_CREATE: %d\n", SYS_CREATE);
+
     thread_exit ();
   }
 }
