0b7461fd6db2fc4a0de9f5ae4f463cbea2ba74ca
andpa149 <andpa149@student.liu.se>
2018-03-08 18:43:58 +0100

    Merged with test

--- linuxpintos/src/userprog/process.h
+++ linuxpintos/src/userprog/process.h
@@ -4,18 +4,6 @@
 #include "threads/thread.h"
 #include "lib/kernel/list.h"
 
-struct start_process_info
-{
-  char* file_name;
-  bool waiting;
-  struct semaphore* p_sp;
-  struct semaphore* c_sp;
-  char* args;
-  struct parent_child_rel* rel;
-  struct thread* parent;
-};
-
-
 tid_t process_execute (const char *file_name);
 int process_wait (tid_t);
 void process_exit (void);
